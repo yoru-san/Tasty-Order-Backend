8a2cbc4649ea92c1f8dc31e8cc3dbd7430d86e52
huntike <logan.lelay@orange.fr>
2018-04-27 15:53:47 +0200

    merge index

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json()); 
 app.use(express.static(__dirname + '/public'));
+app.set('views', (__dirname, '/backoffice/views'));
 
 routes.init(app);
 
-var port = process.env.PORT || 3080;
+var port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
